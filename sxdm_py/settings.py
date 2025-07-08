65ebdd84003ec7063c0708829ba684d1ba12643d
partho1999 <sarker624452@gmail.com>
2025-07-08 17:48:54 +0600

    add the auth check token

--- sxdm_py/settings.py
+++ sxdm_py/settings.py
@@ -133,3 +133,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCESS_AUTH_CHECK_TOKEN = "7VOarATI4IfbqFWLF38VdWoAbHUYlpAY"
