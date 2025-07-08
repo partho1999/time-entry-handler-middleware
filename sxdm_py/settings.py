58eabc7bacec67ded8cd14aa8170c72a3ac77831
partho1999 <sarker624452@gmail.com>
2025-07-08 16:08:29 +0600

    modify settings for deploy

--- sxdm_py/settings.py
+++ sxdm_py/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b&w+3ei$cl1x47gswoqtwai=pr#hm1!hf5%c!1dy#d14e&b7^4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
