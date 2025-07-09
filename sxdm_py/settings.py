cc6b7f150ac980397bd2bab5235ae9a34e1e4f8c
partho1999 <sarker624452@gmail.com>
2025-07-09 17:21:37 +0600

    change setting

--- sxdm_py/settings.py
+++ sxdm_py/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0j=n!y0apgkfl_v38++c)w_4=2ncqs**ij&5#a&x*b)zs8kfsu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
